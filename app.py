7e364d419d43616ed247bfc64fa733daaee97d2f
Victor Wambersie <victor.wambersie@ynov.com>
2024-12-18 16:09:20 +0100

    add port5001

--- app.py
+++ app.py
@@ -84,4 +84,6 @@ def admin():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5001))  # Port par défaut 5000 si non spécifié
+    app.run(host="0.0.0.0", port=port)
